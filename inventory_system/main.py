9d7e4ea40410f7344cd9b15b7112b79d89f25414
Kinerfold <ivakin78123@gmail.com>
2026-06-03 21:34:32 +0300

    fix objects

--- inventory_system/main.py
+++ inventory_system/main.py
@@ -1,14 +1,32 @@
 class Product:
-    def __init__(self, name, price, quantity):
+    
+    def __init__(self, name, price, quantity, inventory):
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.inventory = {
+    "Keyboard": 10,
+    "Mouse": 25,
+    "Monitor": 5
+}
         
         def add_product(self):
-            return "Товар успешно добавлен."
-
+            if inventory == "Keyboard":
+                inventory.append(int)
+                print('Товар добавлен!')
+                
+            if inventory == "Mouse":
+                inventory.append(int)
+                print('Товар добавлен!')
+            
+            if inventory == "Monitor":
+                inventory.append(int)
+                print('Товар добавлен!')
+            else:
+                return 'Товара не существует!'
+        
         def show_products(self):
-            return f"{name}, Цена: {price}, Колво: {quantity}"
+            return inventory
         
         def find_product(self):
             if name:
@@ -17,11 +35,26 @@ class Product:
                 print('Товар не найден')
         
         def delete_product(self):
-             return __dict__.clear(name)
-             print('Товар удалён')
+             if inventory == 'Monitor':
+                 inventory.remove('Monitor')
+             
+             if inventory == 'Mouse':
+                 inventory.remove('Mouse')
+             
+             if inventory == 'Keyboard':
+                 inventory.remove('Keyboard')
+             else:
+                 return 'Товара нет!'
             
         def update_quantity(self):
-            pass
+            if inventory == 'Mouse':
+                inventory.append(int)
+            
+            if inventory == 'Keyboard':
+                inventory.append(int)
+            
+            if inventory == 'Keyboard':
+                inventory.append(int)
     
 class Inventory(Product):
     
