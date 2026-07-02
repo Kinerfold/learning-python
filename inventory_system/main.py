4c8209bffe38dd42aafb3ff3ee52d0009bca989a
Kinerfold <ivakin78123@gmail.com>
2026-07-02 17:44:43 +0300

    fix all def fucntions

--- inventory_system/main.py
+++ inventory_system/main.py
@@ -1,63 +1,46 @@
 class Inventory:
-    
-    inventory = {
-    "Keyboard": 10,
-    "Mouse": 25,
-    "Monitor": 5
-}
-    
-    def __init__(self):
-        pass
+    def __init__(self, inventory):
+        self.inventory = inventory
+        
+        inventory = {
+        "Keyboard": 10,
+        "Mouse": 25,
+        "Monitor": 5
+    }
         
     def add_product(self, name, quantity):
-        self.inventory[name] = quantity
-        
-        if "Keyboard" in self.inventory:
-            self.inventory[name] = quantity
-            print('Товар добавлен!')
-                
-        if "Mouse" in self.inventory:
-            self.inventory[name] = quantity
-            print('Товар добавлен!')
-            
-        if "Monitor" in self.inventory:
+        if name in self.inventory:
             self.inventory[name] = quantity
             print('Товар добавлен!')
         else:
-            return 'Товара не существует!'
+            print('Товар не найден')
         
     def show_products(self):
         for key, value in self.inventory.items():
             print(f"{key}: {value}")
+
+    def find_product(self, name): 
+        if name in self.inventory: 
+            print(f"Найден товар: {name}") 
+        else: 
+            print('Товар не найден')
         
-    def find_product(self, name):
-        def find_product(self, name): 
-            if name in self.inventory: 
-                print(f"Найден товар: {name}") 
-            else: 
-                print('Товар не найден')
-        
-    def delete_product(self):
-            if "Monitor" in self.inventory:
-                del self.inventory["Monitor"]
-             
-            if "Mouse" in self.inventory:
-                del self.inventory["Mouse"]
-             
-            if "Keyboard" in self.inventory:
-                del self.inventory["Keyboard"]
+    def delete_product(self, name):
+            if name in self.inventory:
+                del self.inventory(name)
             else:
-                return 'Товара нет!'
+                print('Товар не найден')
             
-    def update_quantity(self):
-        if "Mouse" in self.inventory:
+    def update_quantity(self, name):
+        if name in self.inventory:
             new_quantity = int(input("Введите количество: "))
-            self.inventory["Mouse"] = new_quantity
-            
-        if "Keyboard" in self.inventory:
+            self.inventory(name) = new_quantity
+        else:
+            print('Товар не найден')
+    
+    def checking_quantity(self, quantity):
+        if quantity > 0:
             new_quantity = int(input("Введите количество: "))
-            self.inventory["Keyboard"] = new_quantity
-            
-        if "Monitor" in self.inventory:
-            new_quantity = int(input("Введите количество: "))
-            self.inventory["Monitor"] = new_quantity
+            self.inventory(quantity) = new_quantity
+        else:
+            print('Неверное количество!')
