4c38e162342063e6e885de6851976757a61512ae
Kinerfold <ivakin78123@gmail.com>
2026-07-03 23:37:31 +0300

    fix __init__ arguments

--- practice/abstract-method.py
+++ practice/abstract-method.py
@@ -13,7 +13,7 @@ class Shape(ABC):
         pass
 
 class Rectangle(Shape):
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
     
@@ -24,7 +24,7 @@ class Rectangle(Shape):
         pass
 
 class Circle(Shape):
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.radius = radius
     
     def area(self):
@@ -34,7 +34,7 @@ class Circle(Shape):
         pass
             
 class Triangle(Shape):
-    def __init__(self, a, b, c):
+    def __init__(self, a=0, b=0, c=0):
         self.a = a
         self.b = b
         self.c = c
@@ -47,34 +47,34 @@ class Triangle(Shape):
 
 # ---------------------------------------------------------------------------------------
 
-# class Animal(ABC):
-#     @abstractmethod
-#     def make_sound(self):
-#         pass
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
 
-# class Dog(Animal):
-#     def make_sound(self):
-#         return 'ГАВ!'
+class Dog(Animal):
+    def make_sound(self):
+        return 'ГАВ!'
 
-# class Cat(Animal):
-#     def make_sound(self):
-#         return 'МЯУ!'
+class Cat(Animal):
+    def make_sound(self):
+        return 'МЯУ!'
 
-# class Bird(Animal):
-#     def make_sound(self):
-#         return 'КАР!'
+class Bird(Animal):
+    def make_sound(self):
+        return 'КАР!'
 
-# dog = Dog()
-# cat = Cat()
-# bird = Bird()
+dog = Dog()
+cat = Cat()
+bird = Bird()
 
-rec = Rectangle()
-circ = Circle()
-trian = Triangle()
+rec = Rectangle(30, 50)
+circ = Circle(30)
+trian = Triangle(10, 10, 10)
 
-# print(dog.make_sound())
-# print(cat.make_sound())
-# print(bird.make_sound())
+print(dog.make_sound())
+print(cat.make_sound())
+print(bird.make_sound())
 
 print(rec.area())
 print(circ.area())
