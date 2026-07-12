005d9f2208c8b84c6010d9a4a0e904b33610f0d4
Kinerfold <ivakin78123@gmail.com>
2026-07-12 20:14:37 +0300

    add more WITH SHELVE

--- practice/files.py
+++ practice/files.py
@@ -1,13 +1,39 @@
 import shelve
 
-FILENAME = 'movies.db'
+FILENAME = 'contacts.db'
 
-user = input('Введите название фильма: ')
-user2 = input('Введите жанр фильма: ')
+menu_user = input('1. Добавить контакт\t2. Найти контакт\t3. Показать все контакты\t4. Выход\n')
 
-with shelve.open(FILENAME) as file:
-    file[user] = user2
+if menu_user == '1':
+    name = input('\tВведите Имя\n')
+    phone_number = input('\tВведите Номер Телефона\n')
     
+    with shelve.open(FILENAME) as file:
+        file[name] = [phone_number]
 
-with shelve.open(FILENAME) as file:
-    print('\nВсе фильмы: ', file[user], '-', file[user2])
+    print('\tИмя: ', name, '\tТелефон: ', phone_number)
+
+if menu_user == '2':
+        number = input('\tВведите номер телефона:\n')
+        
+        with shelve.open(FILENAME) as file:
+            if number in file:
+                print(f"\t{number} {file[name]}")
+            else:
+                print('\tТакого номера нету!')
+
+if menu_user == '3':
+    with shelve.open(FILENAME) as file:
+        keys = sorted(file.keys())
+        
+        for key in keys:
+            print(f'\tИмя: {key} \tТелефон: {file[key]}')
+
+if menu_user == '4':
+    quit()
+
+
+
+
+    
+    
