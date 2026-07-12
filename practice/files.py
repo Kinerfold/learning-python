375599bf2f0fa8a4c25b7ab613543ad712d362eb
Kinerfold <ivakin78123@gmail.com>
2026-07-12 20:35:46 +0300

    fix shelve files

--- practice/files.py
+++ practice/files.py
@@ -4,33 +4,31 @@ FILENAME = 'contacts.db'
 
 menu_user = input('1. Добавить контакт\t2. Найти контакт\t3. Показать все контакты\t4. Выход\n')
 
-if menu_user == '1':
-    name = input('\tВведите Имя\n')
-    phone_number = input('\tВведите Номер Телефона\n')
-    
-    with shelve.open(FILENAME) as file:
+with shelve.open(FILENAME) as file:
+    if menu_user == '1':
+        name = input('\tВведите Имя\n')
+        phone_number = input('\tВведите Номер Телефона\n')
+        
         file[name] = [phone_number]
 
-    print('\tИмя: ', name, '\tТелефон: ', phone_number)
+        print('\tИмя: ', name, '\tТелефон: ', phone_number)
 
-if menu_user == '2':
-        number = input('\tВведите номер телефона:\n')
-        
-        with shelve.open(FILENAME) as file:
-            if number in file:
-                print(f"\t{number} {file[name]}")
+    if menu_user == '2':
+            name = input('\tВведите имя:\n')
+
+            if name in file:
+                print(f"\t{name}: {file[name]}")
             else:
-                print('\tТакого номера нету!')
+                print("\tТакого контакта нету!")
 
-if menu_user == '3':
-    with shelve.open(FILENAME) as file:
-        keys = sorted(file.keys())
-        
-        for key in keys:
-            print(f'\tИмя: {key} \tТелефон: {file[key]}')
+    if menu_user == '3':
+            keys = sorted(file.keys())
+            
+            for key in keys:
+                print(f'\tИмя: {key} \tТелефон: {file[key]}')
 
-if menu_user == '4':
-    quit()
+    if menu_user == '4':
+        quit()
 
 
 
