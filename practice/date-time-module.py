4cda4acadd01146c998fae91afcb0ff0bbddb771
Kinerfold <ivakin78123@gmail.com>
2026-07-15 19:26:38 +0300

    add datetime module

---
+++ practice/date-time-module.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+deadline = datetime.strptime("22/05/2017", "%d/%m/%Y")
+print(deadline)     
+ 
+deadline = datetime.strptime("22/05/2017 12:30", "%d/%m/%Y %H:%M")
+print(deadline)     
+ 
+deadline = datetime.strptime("05-22-2017 12:30", "%m-%d-%Y %H:%M")
+print(deadline)
