dcfdffeade611d82b46877e078df117b5cec2834
Kinerfold <ivakin78123@gmail.com>
2026-07-16 19:40:13 +0300

    add save function

--- auto-uploading-files-with-a-folder/main.py
+++ auto-uploading-files-with-a-folder/main.py
@@ -1,17 +1,29 @@
 from tkinter import *
 from tkinter import ttk
 
+FILENAME = 'data.txt'
+
 def delete():
-    btn2[''] = entry.delete(0, END)
+    entry.delete(0, END)
+
+def save():
+    entry.get()
     
+    with open(FILENAME, 'a', encoding="utf-8") as file:
+        file.write(str(entry.get()))
+        file.write('\n')
+
 root = Tk()
-root.geometry('300x200')
+root.geometry('370x200')
 root.title('Тестовое окно')
 
 entry = ttk.Entry()
-entry.place(x=50, y=70)
+entry.place(x=100, y=70)
 
-btn2 = ttk.Button(text="Сборс", command=delete)
-btn2.place(x=70, y=95)
+btn1 = ttk.Button(text="Сборс", command=delete)
+btn1.place(x=70, y=95)
+
+btn2 = ttk.Button(text='Сохранить', command=save)
+btn2.place(x=180, y=95)
 
 root.mainloop()
