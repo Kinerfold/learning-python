1cd7215941f59842c434b09282ed69488d6d68b6
Kinerfold <ivakin78123@gmail.com>
2026-07-24 14:29:32 +0300

    fix def data

--- auto-uploading-files-with-a-folder/main.py
+++ auto-uploading-files-with-a-folder/main.py
@@ -8,45 +8,45 @@ def delete(): entry.delete(0, END)
 
 def save():
     if not entry.get():
-            label2['text'] = 'Поле пустое!'
-            label2.grid()
-            root.after(2000, label2.grid_remove)
+            empty['text'] = 'Поле пустое!'
+            empty.grid()
+            root.after(2000, empty.grid_remove)
     elif entry.get():
         with open(FILENAME, 'a', encoding="utf-8") as file:
             file.write(entry.get())
             file.write('\n')
             
-            label1['text'] = 'Сохранено!'
-            label1.grid()
-            root.after(2000, label1.grid_remove)
+            savebtn['text'] = 'Сохранено!'
+            savebtn.grid()
+            root.after(2000, savebtn.grid_remove)
 
 def data():
-        # очистка data.txt
-            pass
+    with open(FILENAME, 'w') as file:
+        pass
 
 root = Tk()
 root.geometry('370x200')
 root.title('Тест Tkinter')
 
-label1 = ttk.Label()
-label1.place(x=125, y=45)
+savebtn = ttk.Label()
+savebtn.place(x=125, y=45)
 
-label2 = ttk.Label()
-label2.place(x=135, y=45)
+empty = ttk.Label()
+empty.place(x=135, y=45)
 
-label3 = ttk.Label()
-label3.place(x=165, y=45)
+clear = ttk.Label()
+clear.place(x=165, y=45)
 
 entry = ttk.Entry()
 entry.place(x=100, y=70)
 
-delete_btn = ttk.Button(text="Сборс", command=delete)
-delete_btn.place(x=70, y=95)
+deletebtn = ttk.Button(text="Сборс", command=delete)
+deletebtn.place(x=70, y=95)
 
-save_btn = ttk.Button(text='Сохранить', command=save)
-save_btn.place(x=180, y=95)
+savebtn_savebtnn = ttk.Button(text='Сохранить', command=save)
+savebtn_savebtnn.place(x=180, y=95)
 
-savedata_btn = ttk.Button(text='Список сохранений', command=data)
-savedata_btn.place(x=105, y=125)
+clearbtn = ttk.Button(text='Очистить файл', command=data)
+clearbtn.place(x=115, y=125)
 
 root.mainloop()
