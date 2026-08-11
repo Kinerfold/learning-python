edb4c5107e95bd3923cb2e8cb8994db140ae8c5d
Kinerfold <ivakin78123@gmail.com>
2026-08-11 13:26:23 +0300

    comment

--- auto-uploading-files-with-a-folder/main.py
+++ auto-uploading-files-with-a-folder/main.py
@@ -1,3 +1,5 @@
 import os
 
 os.startfile(r'C:/Users/User/Desktop/Приложения/discord/general (ALT).bat','runas')
+
+# I know the script is very simple, but I spent quite a lot of time figuring out how to make it.
