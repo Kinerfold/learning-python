9fa76221e78819cc014fe98e5f0f5b79df52b908
Kinerfold <ivakin78123@gmail.com>
2026-08-11 19:44:13 +0300

    fix .pack

--- auto-uploading-files-with-a-folder/autofile.py
+++ auto-uploading-files-with-a-folder/autofile.py
@@ -13,15 +13,15 @@ def open_file():
             os.startfile(filepath, 'runas')
             label = ttk.Label(root, text=filepath, font=("Arial", 12))
             label.pack(pady=70)
-            label.after(3000, lambda: label.pack_forget())
+            label.after(5000, lambda: label.pack_forget())
         else:
             return False
 
 root = Tk()
 root.title("Save Files")
-root.geometry("450x400")
+root.geometry("450x500")
 
 open_button = ttk.Button(text="Открыть файл", command=open_file)
-open_button.pack(expand=True, fill=X, padx=[70, 70], pady=30)
+open_button.pack(expand=True, fill=BOTH, padx=[70, 70], pady=130)
 
 root.mainloop()
