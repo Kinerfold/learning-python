f2621efdf6a921fd86ba369fe37e18cb26d2b8b8
Kinerfold <ivakin78123@gmail.com>
2026-08-22 22:01:23 +0300

    add yield practice

---
+++ practice/yield.py
@@ -0,0 +1,46 @@
+# def odd_numbers(n):
+#     for i in range(n):
+#         if i % 2 != 0:
+#             yield i
+            
+# gen = odd_numbers(10)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+
+
+# def squares(numbers):
+#     for i in numbers:
+#         yield i ** 2
+
+# numbers = [2, 4, 6, 8]
+
+# gen = squares(numbers)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+
+
+# def positive_numbers(numbers):
+#     for i in numbers:
+#         if i > 0:
+#             yield i
+
+# numbers = [-5, 10, -3, 0, 7, -1, 15]
+
+# gen = positive_numbers(numbers)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
